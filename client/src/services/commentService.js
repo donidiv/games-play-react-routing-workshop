08dd4d40207e65addeaa3c89620c5d3f800a7e31
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-17 21:37:13 +0200

    Save comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,6 +2,11 @@ import * as request from '../lib/request';
 
 const base_url = 'http://localhost:3030/jsonstore/comments';
 
+export const getAll = async () => {
+    const result = await request.get(base_url);
+    return Object.values(result);
+};
+
 export const create = async (gameId, username, text) => {
     const newComment = await request.post(base_url, {
         gameId,
