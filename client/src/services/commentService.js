0b71b9235a425ede8cd223122952f1935d5d4eb1
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-17 23:02:28 +0200

    Show game own comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,9 +2,14 @@ import * as request from '../lib/request';
 
 const base_url = 'http://localhost:3030/jsonstore/comments';
 
-export const getAll = async () => {
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId='${gameId}'`
+    });
+
     const result = await request.get(base_url);
-    return Object.values(result);
+    //TODO: temp solution until migration to collections service
+    return Object.values(result).filter(comment => comment.gameId === gameId);
 };
 
 export const create = async (gameId, username, text) => {
