1fc7ae0ba379ba9500e2fcf251c834c340b57e35
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-17 18:23:22 +0200

    Modify requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,22 +1,13 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const base_url = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-   const result = await request('GET', base_url);
+   const result = await request.get(base_url);
    return Object.values(result);
 };
 
 export const create = async (gameData) => {
-    const response = await fetch(base_url, {
-        method: 'POST',
-        headers:{
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(gameData)
-    });
-
-    const result = await response.json();
-
+   const result = await request.post(base_url, gameData);
     return result;
 };
