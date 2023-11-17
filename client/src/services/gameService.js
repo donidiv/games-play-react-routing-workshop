6d4d5e3c9f2a359aa7c152a3f8f42c946b905d77
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-17 17:38:31 +0200

    Add get all service method
    Add abstract requests

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,14 @@
-const base_url = 'http://localhost:3030/jsonstore';
+import { request } from "../lib/request";
+
+const base_url = 'http://localhost:3030/jsonstore/games';
+
+export const getAll = async () => {
+   const result = await request('GET', base_url);
+   return Object.values(result);
+};
 
 export const create = async (gameData) => {
-    const response = await fetch(`${base_url}/games`, {
+    const response = await fetch(base_url, {
         method: 'POST',
         headers:{
             'content-type': 'application/json'
