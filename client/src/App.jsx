41d9413b77522bd97ba405d8ded60bbfd80194d6
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-22 13:50:04 +0200

    Add auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,27 +7,31 @@ import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import GameDetails from "./components/game-details/GameDetails";
 import { useState } from "react";
+import AuthContext from "./contexts/AuthContext";
 
 function App() {
-  const[auth, setAuth] = useState({});
+  const [auth, setAuth] = useState({});
 
   const loginSubmitHandler = (values) => {
     console.log(values);
   };
   return (
-    <div id="box">
-      <Header />
+    <AuthContext.Provider value={loginSubmitHandler}>
+      <div id="box">
 
-      <Routes>
-		<Route path="/" element={<Home />}/>
-		<Route path="/games" element={<GameList />}/>
-		<Route path="/games/create" element={<GameCreate />}/>
-		<Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler}/>}/>
-		<Route path="/register" element={<Register />}/>
-		<Route path="/games/:gameId" element={<GameDetails />}/>
-      </Routes>
-      
-    </div>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/games/create" element={<GameCreate />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/games/:gameId" element={<GameDetails />} />
+        </Routes>
+
+      </div>
+    </AuthContext.Provider>
   );
 }
 
