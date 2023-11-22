38582e0b5b2c1815694c9d29aa73c792ead60c10
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-22 23:07:56 +0200

    Use server filtration for comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,15 +1,16 @@
 import * as request from '../lib/request';
 
-const base_url = 'http://localhost:3030/jsonstore/comments';
+const base_url = 'http://localhost:3030/data/comments';
 
 export const getAll = async (gameId) => {
     const query = new URLSearchParams({
-        where: `gameId='${gameId}'`
+        where: `gameId="${gameId}"`
     });
 
-    const result = await request.get(base_url);
+    const result = await request.get(`${base_url}?${query}`);
     //TODO: temp solution until migration to collections service
-    return Object.values(result).filter(comment => comment.gameId === gameId);
+    // return result.filter(comment => comment.gameId === gameId);
+    return result;
 };
 
 export const create = async (gameId, username, text) => {
