bdbfd0b02a8bd2ba457c94cda02e26c745ca3a3b
donidiv <126153831+donidiv@users.noreply.github.com>
2023-11-25 20:30:56 +0200

    persisted state

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,15 +1,52 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Path from "../paths";
+
+import * as authService from '../services/authService';
+import usePersistedState from "../hooks/usePersistedState";
 
 const AuthContext = createContext();
 AuthContext.displayName = 'AuthContext';
 
 export const AuthProvider = ({
     children,
-    value
 }) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+  
+    const loginSubmitHandler = async (values) => {
+      const result = await authService.login(values.email, values.password);
+  
+      setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken);
+      navigate(Path.Home);
+    };
+  
+    const registerSubmitHandler = async (values) => {
+      const result = await authService.register(values.email, values.password);
+  
+      setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken);    
+      navigate(Path.Home);
+    };
+    const logoutHandler = () => {
+      setAuth({});
+      // navigate(Path.Home);
+      localStorage.removeItem('accessToken');
+  
+    };
+    const values = {
+      loginSubmitHandler,
+      registerSubmitHandler,
+      logoutHandler,
+      username: auth.username || auth.email,
+      email: auth.email,
+      isAuthenticated: !!auth.accessToken,
+    };
 
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     );
---
+++ client/src/hooks/usePersistedState.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function usePersistedState(key, defaultedValue) {
+    const[state, setState] = useState(() => {
+        const persistedState = localStorage.getItem(key);
+
+        if (persistedState) {
+            return JSON.parse(persistedState);
+        }
+
+        return defaultedValue;
+    });
+
+    const setPersistedState = (value) => {    
+        setState(value);
+
+        let serializedValue;
+
+        if (typeof value === 'function') {
+            serializedValue = JSON.stringify(value(state));
+        } else {
+            serializedValue = JSON.stringify(value);
+        }         
+
+        localStorage.setItem(key, serializedValue);
+    };
+
+    return [
+        state,
+        setPersistedState,
+    ];
+}
